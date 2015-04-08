71b3b8e5209f0ca6295f3ac406124e7a97974768
Craig <cav9871@saintjoe.edu>
2015-04-08 17:45:26 -0400

    Created a temp file for testing parsing the json

---
+++ craigExam/mm.js
@@ -0,0 +1,7 @@
+var questions = "[\n" + [
+  '{"text": "First state to legalize gay marriage", "answer": "Massachusetts", "decoys": ["California", "Oregon", "Massachusetts", "Mississippi"] }',
+  '{"text": "The last state to join the union", "answer": "Hawaii", "decoys": ["Alaska", "Arizona", "Kansas","Indiana"] }'
+  ].join(",\n ") + "\n]";
+
+//if(typeof module != "undefined" && module.exports)
+  // module.exports = questions;
